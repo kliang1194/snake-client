fe74e4a96a626df286d2e40c6b3476f54122ca53
Kyle <kliang1194@gmail.com>
2021-11-03 15:00:35 -0400

    create wasd movement

--- input.js
+++ input.js
@@ -1,16 +1,34 @@
-const setupInput = function() {
+let connection;
+
+const w = "Move: up";
+const s = "Move: down";
+const a = "Move: left";
+const d = "Move: right";
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+
+  const handleUserInput = (input) => {
+    if (input === '\u0003') {
+      process.exit();
+    } if (input === 'w') {
+      conn.write(w);
+    } if (input === 's') {
+      conn.write(s);
+    } if (input === 'a') {
+      conn.write(a);
+    } if (input === 'd') {
+      conn.write(d);
+    }
+  };
+
   stdin.on("data", handleUserInput);
   return stdin;
 };
 
-const handleUserInput = function(input) {
-  if (input ===  '\u0003') {
-    process.exit();
-  }
-};
 
-module.exports = setupInput; 
+module.exports = {setupInput}; 
