5b7732702e021554bb5f472dfd7564ec8c243d70
Kyle <kliang1194@gmail.com>
2021-11-03 13:28:28 -0400

    send message to server

--- client.js
+++ client.js
@@ -8,6 +8,14 @@ const connect = function () {
 
   conn.setEncoding("utf8");
 
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on('connect', () => {
+    conn.write("Name: KL");
+  });
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
